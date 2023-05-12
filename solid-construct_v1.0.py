3a5b26c59215ebc77e8f8839f2ce2627c424d6d4
pxng0lin <pxng0lin+github@pm.me>
2023-05-12 22:56:48 +0100

    Create solid-construct_v1.0.py
    Python script that searches for constructor functions in solidity files and prints information about the constructors found, including the filename, arguments, and code. It also checks if the code contains a potential 0 address check and prints a warning if it does.

---
+++ solid-construct_v1.0.py
@@ -0,0 +1,28 @@
+import os
+import re
+from colorama import Fore, Style
+
+
+def find_constructors(path):
+    if os.path.isdir(path):
+        files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+        files = [os.path.join(path, f) for f in files if f.endswith(".sol")]
+    else:
+        files = [path]
+    for file in files:
+        with open(file) as f:
+            contents = f.read()
+            matches = re.findall(r"constructor\s*\((.*?)\)\s*\{(.*?)\}", contents, re.DOTALL)
+            for match in matches:
+                args, code = match
+                print(Fore.GREEN + f"Constructor in {os.path.basename(file)}:" + Style.RESET_ALL)
+                if "== address(0)" in code:
+                    print(Fore.RED + "Potential 0 address check exists" + Style.RESET_ALL)
+                print(Fore.YELLOW + f"Arguments: {args.strip()}" + Style.RESET_ALL)
+                print(Fore.BLUE + f"Code: {code.strip()}" + Style.RESET_ALL)
+                print("=" * 30)
+
+
+if __name__ == "__main__":
+    path = input("Enter file path or folder: ")
+    find_constructors(path)
